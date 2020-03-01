b6292a29a0fb32cb1d15af7ee03695df89780808
jiangmichael1 <jiangm1@yahoo.com>
2020-03-01 17:53:08 -0500

    done via vscode

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,107 +1,11 @@
-def introduction (name)
-  puts "Hi, my name is #{name}."
+def introduction(name)
+  puts "Hi, my name is #{name}.\n"  
 end
 
 def introduction_with_language(name,language)
-  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+  puts "Hi, my name is #{name} and I am learning to program in #{language}.\n"
 end
 
 def introduction_with_language_optional(name,language = "Ruby")
-  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+  puts "Hi, my name is #{name} and I am learning to program in #{language}.\n"
 end
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# def introduction(name)
-#  puts "Hi, my name is #{name}."
-#end
-
-#def introduction_with_language(name, language)
-#  puts "Hi, my name is #{name} and I am learning to program in #{language}."
-#end
-
-#def introduction_with_language_optional(name, language="Ruby")
-#  puts "Hi, my name is #{name} and I am learning to program in #{language}."
-#end
